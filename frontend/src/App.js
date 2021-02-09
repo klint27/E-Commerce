e5ffe6316a9207e7f2c533aca912f7f778b48331
unknown <klintsulstarova1997@gmil.com>
2021-02-09 20:16:18 +0100

    new files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './pages/HomeScreen'
 
 const App = () => {
   return (
     <>
-
-      <h1> Welcome to the shop</h1>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
